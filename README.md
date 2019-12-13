1b14f5d777fa23640cbae68ed61b92d85d85f875
Ethan Irvine <edog.irvine@gmail.com>
2019-12-12 23:13:22 -0700

    Created a score function but couldn't figure out how to get it to save in local storage

--- README.md
+++ README.md
@@ -1,32 +1,7 @@
 # Coding-Quiz
 
 
-<!-- this is potential html but is here until I know for sure. (might delete later) -->
+This was by far the hardest homework. I don't know if I wasn't looking up/asking the right questions or if it was because I had very little time compared to other weeks to do it but this assignment definetly was a wake up call to get my act together and not procrastinate as much. More than likely going to come back and get a better grade for this assignment but for now I am mentally drained and just have no urge to keep going on this. 
 
- <div id="q1" class="questions">
-            <h2 class="mt-5">Quistion 1: hfdsuhfjkdshfjksdhfj</h2>
-                <!-- Possible answers for the question -->
-                <div class="row offset-lg-4 offset-md-3 offset-sm-2 mb-1">
-                    <button type="button" class="answers btn btn-primary mr-1">A: hfusdihfaiaushgfiusa</button>
-                    <button type="button" class="answers btn btn-primary">B: gsdfgfjhsdgfisdafksf</button>
-                </div>
-                <div class="row offset-lg-4 offset-md-3 offset-sm-2 mb-5">
-                    <button type="button" class="answers btn btn-primary mr-1">C: hfdjshgfdskjhfjkdshf</button>
-                    <button type="button" class="answers btn btn-primary">D: fhdgsjhfdjskhfjkdshh</button>
-                </div>
-        </div>
-            
-        <div id="q2" class="questions">
-            <h2 class="mt-5">Quistion 2: hfdsuhfjkdshfjksdhfj</h2>
-                <!-- Possible answers for the question -->
-                <div class="row offset-lg-4 offset-md-3 offset-sm-2 mb-1">
-                    <button type="button" class="answers btn btn-primary mr-1">A: hfusdihfaiaushgfiusa</button>
-                    <button type="button" class="answers btn btn-primary">B: gsdfgfjhsdgfisdafksf</button>
-                </div>
-                <div class="row offset-lg-4 offset-md-3 offset-sm-2 mb-5">
-                    <button type="button" class="answers btn btn-primary mr-1">C: hfdjshgfdskjhfjkdshf</button>
-                    <button type="button" class="answers btn btn-primary">D: fhdgsjhfdjskhfjkdshh</button>
-                </div>
-        </div>
-            
-    </div>
+
+https://ethan25irvine.github.io/Coding-Quiz/
--- script.js
+++ script.js
@@ -1,1 +1,110 @@
 
+ 
+ 
+
+  // Questions
+  const questions = [
+  {
+      title: "Commonly used data types DO NOT include:",
+      choices: ["strings", "booleans", "alerts", "numbers"],
+      answer: "alerts"
+      
+  },
+  {
+      title: "The condition in an if / else statement is enclosed within ____.",
+      choices: ["quotes", "curly brackets", "parentheses", "square brackets"],
+      answer: "parentheses"
+  },
+  ];
+ 
+  const totalQ = questions.length;
+  console.log(totalQ);
+  let currentQ = 0;
+  
+//   Below is somehow supposed to get the score to saved in storage but I keep getting NaN and its too late for me to figure it out in time.
+    function saveScore (){
+        localStorage.setItem('theScore', window.score);
+        console.log(window.score);
+    }
+    
+  if(localStorage.getItem('theScore')){ 
+      
+      window.score = localStorage.getItem('theScore')
+  } else {
+      window.score = 0;
+  }
+
+  
+  
+  
+  
+  
+  // to hide blank html when the page loads
+  $(document).ready(function(){
+        $('.mainArea').hide();
+        // this makes it so that the answer for the first question is ready on load
+        localStorage.setItem('answer', questions[0].answer);
+        
+      
+  });
+  
+  $('.qButtons').click(function(){
+      $('.mainArea').show();
+      $('.mainButts').hide();
+      
+      if (currentQ === totalQ){
+          document.location.href = 'enterscore.html';
+          
+      } else {
+          localStorage.setItem('answer', questions[currentQ].answer);
+          $('#question').text(questions[currentQ].title);    
+          $('#option1').text(questions[currentQ].choices[0]);
+          $('#option2').text(questions[currentQ].choices[1]);
+          $('#option3').text(questions[currentQ].choices[2]);
+          $('#option4').text(questions[currentQ].choices[3]); 
+          currentQ++ 
+          
+          
+      }
+  });
+      
+//    This is what changes the html in the index.html page
+  $('.options').click(function(){
+      
+      localStorage.setItem('userChoice',this.textContent);
+      let ans = localStorage.getItem('answer')
+      let userAns = localStorage.getItem('userChoice');
+      if (userAns === ans){
+          saveScore();
+          window.score = Number(window.score)+1;
+          
+          
+      };
+    });
+ 
+  
+  
+ 
+  
+  // takes you to the scoreboard when you press the highscore button
+  $('#highScore').click(function(){
+      document.location.href = 'scoreboard.html';
+  });
+
+  // Timer
+  $('.start').click(function(){
+      localStorage.setItem('time', 75);
+      let timeleft = localStorage.getItem('time');
+      const Timer = setInterval(function(){
+          $("#timer").text(timeleft + " seconds remaining");
+          timeleft -= 1;
+          if(timeleft <= 0){
+              document.location.href = 'enterscore.html';
+          }
+      },1000);     
+  });
+  
+// Below is the enterscore.html script
+
+  
+  
